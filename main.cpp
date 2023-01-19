8f291de0fe4f24c5f2efb804e9c8f85c318bd960
AntonioNotarangelo <antonionitarangelo2004@gmail.com>
2023-01-19 09:43:52 +0100

    evoluzione del progetto
    evoluzione del progetto

--- main.cpp
+++ main.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+int main()
+{
+   /*Scrivere un programma C++che permetta all'utente di inserire la categoria di macchina e i giorni in cui intende affittare l'auto
+    (separati tra loro da spazio); il programma restituisce tutte le auto, se esistono, che soddisfano la richiesta dell'utente.
+     II programma deve stampare il messaggio "Auto non disponibile" se non esistono auto che soddisfano le specifiche dell'utente.
+     Se esistono auto disponibili l'utente deve poter scegliere un veicolo e il programma stampare a video l'elenco aggiornato di tutte le prenotazioni*/
+     ifstream corridoio("auto.csv");
+     string nome_variabile;
+     getline(corridoio, nome_variabile);
+
+
+
+    while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
