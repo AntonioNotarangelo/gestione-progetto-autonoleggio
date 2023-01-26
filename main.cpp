a9d564174078a775d6ed437ad1dec191925f9443
AntonioNotarangelo <antonionitarangelo2004@gmail.com>
2023-01-26 10:01:57 +0100

    evoluzione del programma

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ struct macchina{
 
     string categoria,marca,modello,colore;
 
-     char lunedi,martedi,mercoledi,giovedi,venerdi,sabato,domenica;
+     string lunedi,martedi,mercoledi,giovedi,venerdi,sabato,domenica;
 
 };
 
@@ -22,43 +22,42 @@ int main()
      getline(corridoio, nome_variabile);
      getline(corridoio, nome_variabile);
      macchina appoggio;
+     macchina vettore_delle_auto [100];
+     int numero_macchine=0;
+
     while (!corridoio.eof())
+
     {
         getline(corridoio,appoggio.categoria,',');
         getline(corridoio,appoggio.marca,',');
         getline(corridoio,appoggio.modello,',');
         getline(corridoio,appoggio.colore,',');
-        corridoio>>appoggio.lunedi;
-        corridoio>>appoggio.lunedi;
-        corridoio>>appoggio.martedi;
-        corridoio>>appoggio.martedi;
-        corridoio>>appoggio.martedi;
-        corridoio>>appoggio.mercoledi;
-        corridoio>>appoggio.mercoledi;
-        corridoio>>appoggio.mercoledi;
-        corridoio>>appoggio.giovedi;
-        corridoio>>appoggio.giovedi;
-        corridoio>>appoggio.giovedi;
-        corridoio>>appoggio.venerdi;
-        corridoio>>appoggio.venerdi;
-        corridoio>>appoggio.venerdi;
-        corridoio>>appoggio.venerdi;
-        corridoio>>appoggio.sabato;
-        corridoio>>appoggio.sabato;
-        corridoio>>appoggio.sabato;
-        corridoio>>appoggio.sabato;
-        corridoio>>appoggio.domenica;
-        corridoio>>appoggio.domenica;
-        corridoio>>appoggio.domenica;
+        getline(corridoio,appoggio.lunedi, ',');
+        getline(corridoio,appoggio.martedi, ',');
+        getline(corridoio,appoggio.mercoledi, ',');
+        getline(corridoio,appoggio.giovedi, ',');
+        getline(corridoio,appoggio.venerdi, ',');
+        getline(corridoio,appoggio.sabato, ',');
+        getline(corridoio,appoggio.domenica, '\n');
+        vettore_delle_auto[numero_macchine]=appoggio;
+        numero_macchine++;
 
-        cout<<appoggio.categoria<<appoggio.marca<<appoggio.modello<<appoggio.colore
+
+       /* cout<<appoggio.categoria<<appoggio.marca<<appoggio.modello<<appoggio.colore
         <<appoggio.lunedi<<appoggio.martedi<<appoggio.mercoledi
-        <<appoggio.giovedi<<appoggio.venerdi<<appoggio.sabato<<appoggio.domenica<<endl;//<" "<<appoggio.lunedi;
+        <<appoggio.giovedi<<appoggio.venerdi<<appoggio.sabato<<appoggio.domenica<<endl;//<" "<<appoggio.lunedi; output di prova */
+
+
 
 
     }
-
-
+         string categoria_scelta;
+         string giorni_della_settimana;
+         cout<<" scegli la categoria della macchina";
+         cin>> categoria_scelta;
+         cout<<" Quali giorni desidera affittare la macchina";
+         cin.ignore();
+         getline(cin,giorni_della_settimana)
 
 
 
